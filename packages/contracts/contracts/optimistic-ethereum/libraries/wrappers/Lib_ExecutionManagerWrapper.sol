a22fb053d15f12027938db113d8e60e1d5958cd2
Ben Jones <ben@pseudonym.party>
2021-04-22 23:42:32 -0700

    fix: wrapper naming convention

--- packages/contracts/contracts/optimistic-ethereum/libraries/wrappers/Lib_ExecutionManagerWrapper.sol
+++ packages/contracts/contracts/optimistic-ethereum/libraries/wrappers/Lib_ExecutionManagerWrapper.sol
@@ -121,7 +121,7 @@ library Lib_ExecutionManagerWrapper {
     /**
      * Performs a safe ovmSETCODE call.
      */
-    function safeSETCODE(
+    function ovmSETCODE(
         address _address,
         bytes memory _code
     )
