65baf2533dc9f5099d2b0e472b94f576e7e4a27b
platocrat <jack.abe.winfield@gmail.com>
2021-04-26 14:25:12 -0700

    fix: break examples into single commits

--- examples/Waffle-ERC20-Example/test/getArtifact.js
+++
@@ -1,7 +0,0 @@
-const getArtifact = (target) => {
-  const buildFolder = (target === 'OVM') ? 'build-ovm' : 'build'
-  const ERC20Artifact = require(`../${buildFolder}/ERC20.json`)
-  return ERC20Artifact
-}
-
-module.exports = { getArtifact }
