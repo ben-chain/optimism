1699da4fbe665d01ece4d0502988bc0b59216c99
Ben Jones <ben@pseudonym.party>
2021-04-26 18:29:00 -0700

    improve test

--- integration-tests/test/ovm-self-upgrades.spec.ts
+++ integration-tests/test/ovm-self-upgrades.spec.ts
@@ -6,6 +6,7 @@ import { OptimismEnv } from './shared/env'
 
 import { getContractInterface } from '@eth-optimism/contracts'
 import { l2Provider, OVM_ETH_ADDRESS } from './shared/utils'
+import { ethers } from 'hardhat'
 
 
 // TODO: use actual imported Chugsplash type
@@ -21,48 +22,103 @@ interface SetStorageInstruction {
 	value: string // bytes32
 }
 
+type ChugsplashInstruction = SetCodeInstruction | SetStorageInstruction
+
 // Just an array of the above two instruction types.
-type ChugSplashInstructions = Array<SetCodeInstruction | SetStorageInstruction>
+type ChugSplashInstructions = Array<ChugsplashInstruction>
+
+const isSetStorageInstruction = (instr: ChugsplashInstruction): instr is SetStorageInstruction => {
+  return !instr["code"]
+}
 
 describe('OVM Self-Upgrades', async () => {
   let env: OptimismEnv
   let l2Wallet: Wallet
   let OVM_UpgradeExecutor: Contract
 
+  const applyChugsplashInstructions = async (instructions: ChugSplashInstructions) => {
+    for (const instruction of instructions) {
+      // TODO: remove this try.  The TX errors for the same reason that deposits appear to fail
+      try {  
+        let res
+        if (isSetStorageInstruction(instruction)) {
+          res = await OVM_UpgradeExecutor.setStorage(
+            instruction.target,
+            instruction.key,
+            instruction.value
+          )
+        } else {
+          res = await OVM_UpgradeExecutor.setCode(
+            instruction.target,
+            instruction.code
+          )
+        }
+        await res.wait() // TODO: promise.all
+      } catch (e) {
+        console.log('failedinstruction')
+        console.log(e)
+      }
+    }
+  }
+
+  const checkChugsplashInstructionsApplied = async (instructions: ChugSplashInstructions) => {
+    for (const instruction of instructions) {
+      // TODO: promise.all this for with a map for efficiency
+      if (isSetStorageInstruction(instruction)) {
+        const actualStorage = await l2Provider.getStorageAt(
+          instruction.target,
+          instruction.key
+        )
+        expect(actualStorage).to.deep.eq(instruction.value)
+      } else {
+        const actualCode = await l2Provider.getCode(
+          instruction.target
+        )
+        expect(actualCode).to.deep.eq(instruction.code)
+      }
+    }
+  }
+
+  const applyAndVerifyUpgrade = async (instructions: ChugSplashInstructions) => {
+    await applyChugsplashInstructions(instructions)
+    await checkChugsplashInstructionsApplied(instructions)
+  }
+
   before(async () => {
-    console.log('in before')
     env = await OptimismEnv.new()
     l2Wallet = env.l2Wallet
-    console.log('got env')
-    // OVM_UpgradeExecutor = new Contract(
-    //   '0x420000000000000000000000000000000000000a',
-    //   getContractInterface('OVM_UpgradeExecutor', true),
-    //   l2Wallet
-    // )
+
+    OVM_UpgradeExecutor = new Contract(
+      '0x420000000000000000000000000000000000000a',
+      getContractInterface('OVM_UpgradeExecutor', true),
+      l2Wallet
+    )
   })
 
-  describe('setCode', () => {
-    it('Should estimate gas for ETH transfer', async () => {
-    //   console.log('attempting storage set')
+  describe('setStorage and setCode show up in L2Geth RPC', () => {
+    it('Should execute a basic storage upgrade', async () => {
+      const basicStorageUpgrade: ChugSplashInstructions = [
+        {
+          target: OVM_ETH_ADDRESS,
+          key: '0x1234123412341234123412341234123412341234123412341234123412341234',
+          value: '0x6789123412341234123412341234123412341234123412341234678967896789',
+        }
+      ]
+      await applyAndVerifyUpgrade(basicStorageUpgrade)
+    })
 
-    //   const res = await OVM_UpgradeExecutor.setStorage(
-    //     OVM_ETH_ADDRESS,
-    //     '0x1234123412341234123412341234123412341234123412341234123412341234',
-    //     '0x1234123412341234123412341234123412341234123412341234123412341234'
-    //   )
+    it.only('Should execute a basic code upgrade', async () => {
+      const DummyContract = await (
+        await ethers.getContractFactory('SimpleStorage', l2Wallet)
+      ).deploy()
 
-    //   console.log('attempted storage set')
-
-    //   await res.wait()
-
-    //   console.log('waited for storage set')
-
-    //   const newStorage = await l2Provider.getStorageAt(
-    //     OVM_ETH_ADDRESS,
-    //     '0x1234123412341234123412341234123412341234123412341234123412341234'
-    //   )
-
-    //   console.log('new storage is', newStorage)
-    // })
+      const basicStorageUpgrade: ChugSplashInstructions = [
+        {
+          target: DummyContract.address,
+          code: '0x1234123412341234123412341234123412341234123412341234123412341234',
+        }
+      ]
+      await applyAndVerifyUpgrade(basicStorageUpgrade)
+    })
   })
 })
