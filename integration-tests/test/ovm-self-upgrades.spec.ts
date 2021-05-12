12d3b00431b70b88d009e0cdc1b67711c85eb399
Ben Jones <ben@pseudonym.party>
2021-05-11 20:06:15 -0700

    making progress, messed up the npm

--- integration-tests/test/ovm-self-upgrades.spec.ts
+++ integration-tests/test/ovm-self-upgrades.spec.ts
@@ -1,37 +1,13 @@
 import { expect } from 'chai'
-import { Wallet, utils, BigNumber, Contract, ContractFactory } from 'ethers'
+import { ethers } from 'hardhat'
+import { Wallet, Contract, ContractFactory } from 'ethers'
+import { getContractInterface, ChugSplashAction, isSetStorageAction } from '@eth-optimism/contracts'
 
 import { OptimismEnv } from './shared/env'
+import { l1Provider, l2Provider, OVM_ETH_ADDRESS } from './shared/utils'
+import newExecutionManagerJson from '../artifacts/contracts/ovm-self-upgrades/NewExecutionManager.sol/NewExecutionManager.json'
 
-import { getContractInterface } from '@eth-optimism/contracts'
-import { l2Provider, OVM_ETH_ADDRESS } from './shared/utils'
-import { ethers } from 'hardhat'
-
-// TODO: use actual imported Chugsplash type
-
-interface SetCodeInstruction {
-  target: string // address
-  code: string // bytes memory
-}
-
-interface SetStorageInstruction {
-  target: string // address
-  key: string // bytes32
-  value: string // bytes32
-}
-
-type ChugsplashInstruction = SetCodeInstruction | SetStorageInstruction
-
-// Just an array of the above two instruction types.
-type ChugSplashInstructions = Array<ChugsplashInstruction>
-
-const isSetStorageInstruction = (
-  instr: ChugsplashInstruction
-): instr is SetStorageInstruction => {
-  return !instr['code']
-}
-
-describe('OVM Self-Upgrades', async () => {
+describe.only('OVM Self-Upgrades', async () => {
   let env: OptimismEnv
   let l2Wallet: Wallet
   let OVM_UpgradeExecutor: Contract
@@ -40,11 +16,11 @@ describe('OVM Self-Upgrades', async () => {
   let Factory__SimpleStorage: ContractFactory
 
   const applyChugsplashInstructions = async (
-    instructions: ChugSplashInstructions
+    instructions: ChugSplashAction[]
   ) => {
     for (const instruction of instructions) {
       let res
-      if (isSetStorageInstruction(instruction)) {
+      if (isSetStorageAction(instruction)) {
         res = await OVM_UpgradeExecutor.setStorage(
           instruction.target,
           instruction.key,
@@ -61,11 +37,11 @@ describe('OVM Self-Upgrades', async () => {
   }
 
   const checkChugsplashInstructionsApplied = async (
-    instructions: ChugSplashInstructions
+    instructions: ChugSplashAction[]
   ) => {
     for (const instruction of instructions) {
       // TODO: promise.all this for with a map for efficiency
-      if (isSetStorageInstruction(instruction)) {
+      if (isSetStorageAction(instruction)) {
         const actualStorage = await l2Provider.getStorageAt(
           instruction.target,
           instruction.key
@@ -79,7 +55,7 @@ describe('OVM Self-Upgrades', async () => {
   }
 
   const applyAndVerifyUpgrade = async (
-    instructions: ChugSplashInstructions
+    instructions: ChugSplashAction[]
   ) => {
     await applyChugsplashInstructions(instructions)
     await checkChugsplashInstructionsApplied(instructions)
@@ -100,9 +76,8 @@ describe('OVM Self-Upgrades', async () => {
       'ReturnTwo',
       l2Wallet
     )
-    const returnTwo = await (
-      await Factory__ReturnTwo.deploy()
-    ).deployTransaction.wait()
+    const returnTwo: Contract = await Factory__ReturnTwo.deploy()
+    await returnTwo.deployTransaction.wait()
     DeployedBytecode__ReturnTwo = await l2Provider.getCode(
       returnTwo.contractAddress
     )
@@ -115,7 +90,7 @@ describe('OVM Self-Upgrades', async () => {
 
   describe('setStorage and setCode are correctly applied according to geth RPC', () => {
     it('Should execute a basic storage upgrade', async () => {
-      const basicStorageUpgrade: ChugSplashInstructions = [
+      const basicStorageUpgrade: ChugSplashAction[] = [
         {
           target: OVM_ETH_ADDRESS,
           key:
@@ -133,7 +108,7 @@ describe('OVM Self-Upgrades', async () => {
       ).deploy()
       await DummyContract.deployTransaction.wait()
 
-      const basicCodeUpgrade: ChugSplashInstructions = [
+      const basicCodeUpgrade: ChugSplashAction[] = [
         {
           target: DummyContract.address,
           code:
@@ -144,9 +119,7 @@ describe('OVM Self-Upgrades', async () => {
     })
 
     it('Should execute a basic code upgrade which is not overwriting an existing account', async () => {
-      // TODO: fix me?  Currently breaks due to nil pointer dereference; triggerd by evm.StateDB.SetCode(...) in ovm_state_manager.go ?
-      // More recent update: I cannot get this to error out any more.
-      const emptyAccountCodeUpgrade: ChugSplashInstructions = [
+      const emptyAccountCodeUpgrade: ChugSplashAction[] = [
         {
           target: '0x5678657856785678567856785678567856785678',
           code:
@@ -166,7 +139,7 @@ describe('OVM Self-Upgrades', async () => {
       expect(valueBefore).to.eq(ethers.constants.HashZero)
 
       const newValue = '0x' + '00'.repeat(31) + '01'
-      const storageVarUpgrade: ChugSplashInstructions = [
+      const storageVarUpgrade: ChugSplashAction[] = [
         {
           target: SimpleStorage.address,
           key: ethers.constants.HashZero,
@@ -186,7 +159,7 @@ describe('OVM Self-Upgrades', async () => {
       const one = await Returner.get()
       expect(one.toNumber()).to.eq(1)
 
-      const constantUpgrade: ChugSplashInstructions = [
+      const constantUpgrade: ChugSplashAction[] = [
         {
           target: Returner.address,
           code: DeployedBytecode__ReturnTwo,
@@ -197,6 +170,17 @@ describe('OVM Self-Upgrades', async () => {
 
       const two = await Returner.get()
       expect(two.toNumber()).to.eq(2)
+    })
+  })
+
+  describe('L2 virtualization contracts can themselves be upgraded', async () => {
+    it('does a basic upgrade to the Execution Manager', async () => {
+      const executionManagerUpgrade: ChugSplashAction = {
+        target: '0xdeaddeaddeaddeaddeaddeaddeaddeaddead0005',
+        code: newExecutionManagerJson.deployedBytecode
+      }
+
+      await applyAndVerifyUpgrade([executionManagerUpgrade])
     })
   })
 })
