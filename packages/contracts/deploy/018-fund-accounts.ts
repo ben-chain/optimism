612916b524299056cf3ffd0fb8b99edd30826c48
Kevin Ho <kevinjho1996@gmail.com>
2021-06-03 18:46:21 -0700

    feat(contracts): add funding L2 ETH step to deploy script

---
+++ packages/contracts/deploy/018-fund-accounts.ts
@@ -0,0 +1,37 @@
+/* Imports: External */
+import { DeployFunction } from 'hardhat-deploy/dist/types'
+
+/* Imports: Internal */
+import { getDeployedContract } from '../src/hardhat-deploy-ethers'
+import { ethers } from 'hardhat'
+
+const deployFn: DeployFunction = async (hre) => {
+  const { deployer } = await hre.getNamedAccounts()
+
+   // Fund all default hardhat signers  with ETH if deploying on a local hardhat network
+   const { chainId } = await ethers.provider.getNetwork()
+   if (chainId === 31337) {
+     const Proxy__OVM_L1ETHGateway = await getDeployedContract(
+       hre,
+       'Proxy__OVM_L1ETHGateway',
+       {
+         signerOrProvider: deployer,
+         iface: 'OVM_L1ETHGateway',
+       }
+     )
+     const signers = await ethers.getSigners()
+     for (const signer of signers) {
+       const to = await signer.getAddress()
+       const l2gas = 8_000_000
+       const amount = '100'
+       const value = ethers.utils.parseEther(amount)
+       await Proxy__OVM_L1ETHGateway.depositTo(to, l2gas, '0x', { value })
+       console.log(`✓ Funded ${to} on L2 with ${amount} ETH`)
+     }
+   }
+}
+
+deployFn.dependencies = ['Proxy__OVM_L1ETHGateway']
+deployFn.tags = ['fund-accounts']
+
+export default deployFn
